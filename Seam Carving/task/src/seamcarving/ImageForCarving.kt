b18592efe18c494dfc9ced45384187a641bbc61a
PavleMomirovic <pakito@elfak.rs>
2021-10-20 23:02:52 +0200

    Initial commit

---
+++ Seam Carving/task/src/seamcarving/ImageForCarving.kt
@@ -0,0 +1,73 @@
+package seamcarving
+
+import com.sun.tools.jdeps.Graph
+import java.awt.Color
+import java.awt.image.BufferedImage
+import java.awt.image.IndexColorModel
+import java.io.File
+import java.lang.NullPointerException
+import java.util.*
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+class ImageForCarving(private val im : BufferedImage) {
+
+    val intensityMat = Array<Array<Int>>(im.width) { Array<Int>(im.height) { 0 } }
+
+    fun pixelEnergy(x: Int, y: Int): Double {
+        var xm = x
+        var ym = y
+        if (xm == 0) xm++
+        if (xm == im.width - 1) xm--
+        val dx = ((Color(im.getRGB(xm + 1, y)).red - Color(im.getRGB(xm - 1, y)).red).toDouble()).pow(2.0) +
+                ((Color(im.getRGB(xm + 1, y)).green - Color(im.getRGB(xm - 1, y)).green).toDouble()).pow(2.0) +
+                ((Color(im.getRGB(xm + 1, y)).blue - Color(im.getRGB(xm - 1, y)).blue).toDouble()).pow(2.0)
+        if (y == 0) ym++
+        if (y == im.height - 1) ym--
+        val dy = ((Color(im.getRGB(x, ym + 1)).red - Color(im.getRGB(x, ym - 1)).red).toDouble()).pow(2.0) +
+                ((Color(im.getRGB(x, ym + 1)).green - Color(im.getRGB(x, ym - 1)).green).toDouble()).pow(2.0) +
+                ((Color(im.getRGB(x, ym + 1)).blue - Color(im.getRGB(x, ym - 1)).blue).toDouble()).pow(2.0)
+
+        return sqrt(dx + dy)
+    }
+
+    fun grayScaleEnergyRepresentation(): BufferedImage {
+        val out: BufferedImage = im
+        makeIntensityMatrix()
+        for (i in 0 until im.width) {
+            for (j in 0 until im.height) {
+                val k = intensityMat[i][j]
+                out.setRGB(i, j, Color(k,k,k).rgb)
+            }
+        }
+        return out
+    }
+
+    fun makeIntensityMatrix(){
+        var maxEn = 0.0
+        val helpMat = Array<Array<Double>>(im.width) { Array<Double>(im.height) { 0.0 } }
+
+
+        //calculating energy and preparing:
+        for (i in 0 until im.width) {
+            for (j in 0 until im.height) {
+                helpMat[i][j] = pixelEnergy(i, j)
+                if (helpMat[i][j] > maxEn) maxEn = helpMat[i][j]
+            }
+        }
+
+        // normalization and making:
+        for (i in 0 until im.width) {
+            for (j in 0 until im.height) {
+                intensityMat[i][j] = (255.0 * helpMat[i][j] / maxEn).toInt()
+            }
+        }
+
+    }
+    val m = im.width
+    val n = im.height
+    val pq = PriorityQueue<Pixel>(m*n)
+
+
+
+}
---
+++ Seam Carving/task/src/seamcarving/Main.kt
@@ -0,0 +1,39 @@
+package seamcarving
+
+import java.awt.Color
+import java.awt.image.BufferedImage
+import java.io.File
+import java.lang.Math.*
+import java.util.*
+import javax.imageio.ImageIO
+
+fun negative(im: BufferedImage) {
+    for (i in 0 until im.width) {
+        for (j in 0 until im.height) {
+            val tmp = im.getRGB(i, j)
+            val col = Color(255 - Color(tmp, true).red, 255 - Color(tmp, true).green, 255 - Color(tmp, true).blue)
+            im.setRGB(i, j, col.rgb)
+        }
+    }
+}
+fun drawDiagonals(im: BufferedImage) {
+    val g2 = im.createGraphics()
+    g2.color = Color.RED
+    val x = im.width
+    val y = im.height
+    g2.drawLine(0, 0, x - 1, y - 1)
+    g2.drawLine(0, y - 1, x - 1, 0)
+}
+
+
+
+
+fun main(args: Array<String>) {
+    val fi = File(args[1])
+    val fo = File(args[3])
+
+    val image: BufferedImage = ImageIO.read(fi)
+    val im = ImageForCarving(image)
+    ImageIO.write(im.grayScaleEnergyRepresentation(), "png", fo)
+}
+
